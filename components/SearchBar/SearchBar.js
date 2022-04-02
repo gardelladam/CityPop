3cc86b7d907fe24fba8e3afe3b0cbeca8e403653
Adam Gardell <adaga376@student.liu.se>
2022-04-02 15:46:38 +0200

    Added searchbar

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,71 @@
+// SearchBar, taken from https://blog.logrocket.com/create-react-native-search-bar-from-scratch/
+
+import React from "react";
+import { StyleSheet, TextInput, View, Keyboard, Button } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
+import styles from "./styles";
+
+const SearchBar = (props) => {
+  const {
+    toolTip,
+    clicked,
+    searchPhrase,
+    setSearchPhrase,
+    setClicked,
+    submit,
+    setSubmit,
+  } = props;
+  return (
+    <View style={styles.container}>
+      <View
+        style={
+          clicked ? styles.searchBar__clicked : styles.searchBar__unclicked
+        }
+      >
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder={toolTip}
+          value={searchPhrase}
+          onChangeText={setSearchPhrase}
+          onSubmitEditing={() => setSubmit(true)}
+          onFocus={() => {
+            setClicked(true);
+          }}
+        />
+        {/* cross Icon, depending on whether the search bar is clicked or not */}
+        {clicked && (
+          <Entypo
+            name="cross"
+            size={20}
+            color="black"
+            style={{ padding: 1 }}
+            onPress={() => {
+              setSearchPhrase("");
+            }}
+          />
+        )}
+      </View>
+      {/* cancel button, depending on whether the search bar is clicked or not */}
+      {clicked && (
+        <View>
+          <Button
+            title="Cancel"
+            onPress={() => {
+              Keyboard.dismiss();
+              setClicked(false);
+            }}
+          ></Button>
+        </View>
+      )}
+    </View>
+  );
+};
+export default SearchBar;
--- screens/SearchScreen/SearchScreen.js
+++ screens/SearchScreen/SearchScreen.js
@@ -1,26 +1,49 @@
 import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CustomButton from "../../components/CustomButton/CustomButton.js";
+import SearchBar from "../../components/SearchBar/SearchBar.js";
 import styles from "./styles";
 import globalStyles from "../../globalStyles";
 
 const SearchScreen = ({ route, navigation }) => {
   const { type } = route.params;
 
-  const title = type === 1 ? "SEARCH BY CITY" : "SEARCH BY COUNTRY";
-  const tooltip = type === 1 ? "Enter a city" : "Enter a country";
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [submit, setSubmit] = useState(false);
 
-  return (
-    <SafeAreaView style={globalStyles.appContainer}>
-      <View style={globalStyles.topBar}>
-        <CustomButton title="Back" onPress={() => navigation.goBack()} />
-      </View>
+  const title = type === 1 ? "SEARCH BY CITY" : "SEARCH BY COUNTRY";
+  const toolTip = type === 1 ? "Enter a city" : "Enter a country";
+
+  if (submit === true) {
+    return (
       <View style={globalStyles.titleContainer}>
-        <Text style={globalStyles.title}>{title}</Text>
+        <Text style={globalStyles.title}>{searchPhrase}</Text>
       </View>
-      <View style={globalStyles.content}></View>
-    </SafeAreaView>
-  );
+    );
+  } else {
+    return (
+      <SafeAreaView style={globalStyles.appContainer}>
+        <View style={globalStyles.topBar}>
+          <CustomButton title="Back" onPress={() => navigation.goBack()} />
+        </View>
+        <View style={globalStyles.titleContainer}>
+          <Text style={globalStyles.title}>{title}</Text>
+        </View>
+        <View style={globalStyles.content}>
+          <SearchBar
+            toolTip={toolTip}
+            searchPhrase={searchPhrase}
+            setSearchPhrase={setSearchPhrase}
+            clicked={clicked}
+            setClicked={setClicked}
+            submit={submit}
+            setSubmit={setSubmit}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 };
 
 export default SearchScreen;
